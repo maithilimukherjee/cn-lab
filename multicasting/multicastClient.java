6fa3a65a69bc10e51d257d0db190c4b8017cce55
maithili <86041684+maithilimukherjee@users.noreply.github.com>
2026-07-18 00:29:18 +0530

    Fix multicast group address and improve socket usage
    Updated multicast group address and socket handling.

--- multicasting/multicastClient.java
+++ multicasting/multicastClient.java
@@ -1,39 +1,38 @@
-import java.net.DatagramPacket;     // Added missing import
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.MulticastSocket;
-import java.net.NetworkInterface;
-import java.net.SocketAddress;
+import java.net.*;
 
 public class multicastClient
 {
     public static void main(String args[])
     {
-        System.out.println("Multicast Client is running...");
-
         try
         {
             int port = 4446;
+
+            InetAddress group = InetAddress.getByName("231.0.0.1");
+
             MulticastSocket socket = new MulticastSocket(port);
 
-            InetAddress group = InetAddress.getByName("230.0.0.1");
-
-            SocketAddress groupAddress = new InetSocketAddress(group, port);
-
-            NetworkInterface netIf = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
-            socket.joinGroup(groupAddress, netIf);
+            socket.joinGroup(group);
 
             byte[] buffer = new byte[256];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+            DatagramPacket packet =
+                new DatagramPacket(buffer, buffer.length);
 
             socket.receive(packet);
-            String received = new String(packet.getData(), 0, packet.getLength());
-            System.out.println("Received message: " + received);
 
-            socket.leaveGroup(groupAddress, netIf);
+            String message =
+                new String(packet.getData(), 0, packet.getLength());
+
+            System.out.println(
+                "Received message from multicast group: "
+                + message);
+
+            socket.leaveGroup(group);
+
             socket.close();
         }
-        catch (Exception e)
+        catch(Exception e)
         {
             e.printStackTrace();
         }
